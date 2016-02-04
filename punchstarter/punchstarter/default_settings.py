21c99aaa263783634904c7a6a9ee27e709b2533d
Fernando Valdes <fernando@valdes.com.mx>
2016-02-03 22:50:00 -0600

    Uploading a cover photo

--- punchstarter/punchstarter/default_settings.py
+++ punchstarter/punchstarter/default_settings.py
@@ -1,5 +1,13 @@
 import os
+import cloudinary
 
 DEBUG=True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + BASE_DIR + "/app.db"
+
+
+cloudinary.config(
+  cloud_name = "fvo2006",
+  api_key = "153618543686195",
+  api_secret = "gso1RKZn740oV1RagH8begwo2TY"
+)
